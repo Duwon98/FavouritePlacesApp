50cc42c9aa51516a88785a487bda09845df1f487
Duwon Ha <duwon.ha@griffithuni.edu.au>
2022-05-02 16:49:43 +1000

    Added some views

---
+++ FavouritePlaces/FavouritePlaces+ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FavouritePlaces+ViewModel.swift
+//  FavouritePlaces
+//
+//  Created by Duwon Ha on 2/5/2022.
+//
+
+import Foundation
+
+extension FavouritePlaces{
+    var placeArray: [Place] {
+        
+    }
+}
