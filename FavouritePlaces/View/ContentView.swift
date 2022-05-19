b56be91367652b78e54ed1c1663517f2f3850542
Duwon Ha <duwon.ha@griffithuni.edu.au>
2022-05-20 09:56:57 +1000

    Made a function to make a demo places(default data) if core data is empty

--- FavouritePlaces/View/ContentView.swift
+++ FavouritePlaces/View/ContentView.swift
@@ -39,7 +39,12 @@ var body: some View {
                     }
                 }
             }
+    }.onAppear(){
+        /// Checking if coredata is empty
+        if favPlaces.isEmpty{
+            loadDefaultData()
         }
+    }
     }
     /// <#Description#>
     /// This function will allow users to add a Favourite Place
@@ -73,6 +78,48 @@ var body: some View {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+        }
+    }
+    /// <#Description#>
+    /// This function will be called if the coredata is empty. It will generate the default data.
+    private func loadDefaultData() {
+        let defaultPlace = FavouritePlaces(context: viewContext)
+        defaultPlace.name = "Busan"
+        defaultPlace.latitude = 35.16
+        defaultPlace.longitude = 129.14
+        defaultPlace.placeNote = "This is my hometown Haeundae"
+        defaultPlace.placeURL = "https://www.lottehotelmagazine.com/resources/d434c17f-5ac2-4b98-8021-f3bdd5cc26f4_img_TRAVEL_busan_detail01.jpg"
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        
+        let defaultPlace2 = FavouritePlaces(context: viewContext)
+        defaultPlace2.name = "Gold Coast"
+        defaultPlace2.latitude = -28
+        defaultPlace2.longitude = 153.42
+        defaultPlace2.placeNote = "This is Surfers Paradise"
+        defaultPlace2.placeURL = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/27/77/5b/photo0jpg.jpg?w=1200&h=1200&s=1"
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        
+        let defaultPlace3 = FavouritePlaces(context: viewContext)
+        defaultPlace3.name = "Paris"
+        defaultPlace3.latitude = 48.8
+        defaultPlace3.longitude = 2.29
+        defaultPlace3.placeNote = "This is Eiffel Tower!"
+        defaultPlace3.placeURL = "https://www.mycity24.com.au/mycityko/pad_img/72100_1.jpg"
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
